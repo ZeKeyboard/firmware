1a9d905a3b4466a7d4f4f8da15aad1d905c4bdd6
Malcolm Vigren <trekommafem2@gmail.com>
2024-03-28 17:07:41 +0100

    Remove multithreading from simulator

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -1,25 +1,6 @@
 #include "simulator_device.h"
 #include "../core/firmware.h"
 #include "simulator_window.h"
-#include <thread>
-
-
-void window_thread(simulator::SimulatorWindow& window)
-{
-    while (window.is_open())
-    {
-        window.update();
-    }
-}
-
-
-void firmware_thread(core::Firmware& firmware)
-{
-    while (true)
-    {
-        firmware.update();
-    }
-}
 
 
 int main()
@@ -27,11 +8,11 @@ int main()
     simulator::SimulatorDevice device;
     core::Firmware firmware{device};
     simulator::SimulatorWindow window{device, firmware};
-
-    std::thread window_thread{::window_thread, std::ref(window)};
-    std::thread firmware_thread{::firmware_thread, std::ref(firmware)};
-    window_thread.join();
-    firmware_thread.join();
+    while (window.is_open())
+    {
+        window.update();
+        firmware.update();
+    }
 
     return 0;
 }
